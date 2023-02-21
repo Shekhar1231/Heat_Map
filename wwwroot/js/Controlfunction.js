fc62aa8f05b2a100e175898bcf21004b37040838
Shekhar1231 <Shekhar.Madheshiya@wsp.com>
2023-02-21 11:06:37 +0530

    update all changes

--- wwwroot/js/Controlfunction.js
+++ wwwroot/js/Controlfunction.js
@@ -1,5 +1,7 @@
-﻿$(document).ready(function () {
+﻿    import { initViewer, loadModel } from './viewer.js';
     const Menuelement = document.getElementById("MenuPanel");
+
+
     $('#Menubarbutton').click(function () {
         if (Menuelement.style.display !== "none") {
             Menuelement.style.transition = "1s"
@@ -27,11 +29,58 @@
                 throw new Error(await resp.text());
             }
         } catch (e) {
-            upload.removeAttribute('disabled');            
+            upload.removeAttribute('disabled');
         }
     }
 
-});
+    const transalteModel = document.getElementById("translateModel");
+
+    transalteModel.onclick = async () => {
+       
+        initViewer(document.getElementById('preview')).then(viewer => {
+            const urn = window.location.hash?.substring(1);
+
+        });
+        onModelSelected(viewer, urn);
+    }
+
+    
+
+    async function onModelSelected(viewer, urn) {
+        if (window.onModelSelectedTimeout) {
+            clearTimeout(window.onModelSelectedTimeout);
+            delete window.onModelSelectedTimeout;
+        }
+        window.location.hash = urn;
+        try {
+            const resp = await fetch(`/api/models/${urn}/status`);
+            if (!resp.ok) {
+                throw new Error(await resp.text());
+            }
+            const status = await resp.json();
+            switch (status.status) {
+                case 'n/a':
+                    showNotification(`Model has not been translated.`);
+                    break;
+                case 'inprogress':
+                    showNotification(`Model is being translated (${status.progress})...`);
+                    window.onModelSelectedTimeout = setTimeout(onModelSelected, 5000, viewer, urn);
+                    break;
+                case 'failed':
+                    showNotification(`Translation failed. <ul>${status.messages.map(msg => `<li>${JSON.stringify(msg)}</li>`).join('')}</ul>`);
+                    break;
+                default:
+                    clearNotification();
+                    loadModel(viewer, urn);
+                    break;
+            }
+        } catch (err) {
+            alert('Could not load model. See the console for more details.');
+            console.error(err);
+        }
+    }
+
+
         
             
 
